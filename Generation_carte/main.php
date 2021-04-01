df85431cacda218a6391d3b092befc77f0744cc2
Your Name <you@example.com>
2021-04-01 15:01:17 +0200

    Head fait

---
+++ Generation_carte/main.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Map</title>
+    <meta name="Description" content="Génération d'une carte HTML aléatoire" />
+    <link href="css/bootstrap-tutoriels.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+
+    </body>
+</html>
